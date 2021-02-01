bc7ed48cc5d281ebdc8862782d5e9f569a029e1d
Gaurang <delvadiya.gaurang2512@gmail.com>
2021-02-01 18:32:56 +0530

    Added github url in setup file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="This package will help to perform Rabbit MQ operations",
     long_description=long_description,
     long_description_content_type="text",
-    url="",
+    url="https://github.com/gaurang19990412/message-broker",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python >= 3.5",
